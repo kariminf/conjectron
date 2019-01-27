0c64954075c9f024a0004c26d18fe2ec078724ab
kariminf <kariminfo0@gmail.com>
2019-01-27 08:41:59 +0100

    add settings; fixes #1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-JsLingua = require("jslingua");
+const jslingua = require("jslingua");
+
+const settings = require("settings-store");
+
+settings.init({
+    appName:       "Conjectron", //required,
+    publisherName: "kariminf", //optional
+    reverseDNS:    "kariminf.conjectron" //required for macOS
+});
+
+let lang = settings.value("lang", "ar");
+
+document.getElementById("lang").value = lang;
--- titlebar/titlebar.js
+++ titlebar/titlebar.js
@@ -1,8 +1,24 @@
 const remote = require("electron").remote;
 const fs = require("fs");
+const settings = require("settings-store");
 
+//Load titlebar HTML content
 let content = fs.readFileSync(__dirname + "/titlebar.html", "utf8");
 document.getElementById("titlebar").innerHTML = content;
+
+//Load titlebar CSS content
+let cssId = "titlebar-css";
+if (!document.getElementById(cssId))
+{
+    let head  = document.getElementsByTagName("head")[0];
+    var link  = document.createElement("link");
+    link.id   = cssId;
+    link.rel  = "stylesheet";
+    link.type = "text/css";
+    link.href = "./titlebar/titlebar.css";
+    link.media = "all";
+    head.appendChild(link);
+}
 
 document.getElementById("min-btn").addEventListener("click", function (e) {
        let window = remote.getCurrentWindow();
@@ -19,6 +35,7 @@ document.getElementById("max-btn").addEventListener("click", function (e) {
   });
 
 document.getElementById("close-btn").addEventListener("click", function (e) {
+    settings.setValue("lang", document.getElementById("lang").value);
       let window = remote.getCurrentWindow();
       window.close();
   });
